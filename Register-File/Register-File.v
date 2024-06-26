543f372b77e3237d92cf842fbf690c20e14fa735
Amr Jamal <amr.elshamshery@ejust.edu.eg>
2024-06-26 23:45:06 +0300

    Added register file

---
+++ Register-File/Register-File.v
@@ -0,0 +1,22 @@
+module registerFile (
+    input rst, clk, writeEnable,
+    input [31 : 0] dataWrite, addressWrite, addressA, addressB,
+    output reg [31 : 0] dataA, dataB
+);
+    integer  i;
+    reg [31 : 0] registerFileData [31 : 0];
+
+    always @(posedge clk, posedge rst) begin
+        if(rst)
+            for(i = 0; i < 32; i++)
+                registerFileData[i] <= 0;
+        else if(writeEnable)
+            registerFileData[addressWrite] = dataWrite;
+    end
+
+    always@(*)begin
+        dataA = registerFileData[addressA];
+        dataB = registerFileData[addressB];
+    end
+
+endmodule
